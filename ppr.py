cb971e2f2451b79db10ed7a05c3738fd5519adcd
Bryan Weber <Looker@Bryan-Webers-MacBook-Air.local>
2018-02-03 15:53:31 -0800

    colors merges vertical alignment

--- ppr.py
+++ ppr.py
@@ -39,44 +39,54 @@ df = pandas.read_json(json_query)
 
 #wb = openpyxl.load_workbook('testfile.xlsx')
 
-# OPEN WORKBOOK IN MEMORY BY CALLING THE ATTACHMENT>>DATA AND DECODING
 
-bg_rgb = ['68BD45', '329bd6','ffe300']
-bg_color = ('FF68BD45', 'FF329bd6','FFffe300')
-
-def colorFader(n,)
 
 def checkValues(value1,value2):
     return value1 == value2
 
+bg_rgb = ['68BD45', '329bd6','ffe300']
+bg_rgb_len = len(bg_rgb)
+bg_argb = ["FF" + str(x) for x in bg_rgb]
+
 wb = openpyxl.load_workbook(filename=BytesIO(base64.b64decode(df.attachment['data'])))
+# OPEN WORKBOOK IN MEMORY BY CALLING THE ATTACHMENT>>DATA AND DECODING
 ws = wb.active
 
 rows = ws.max_row
 column = ws.max_column
+first_data_row = {"index":2, "cell":3} # index, cell
+first_data_col = {"index":3, "cell":'D'}
+last_data_row = {'index':68,'cell':69}
+last_data_col = {'index':74,'cell':'BW'}
 
 cells = ws[2]
-for i in range(2,column):
+for i in range(first_data_col['index'],column):
     cells[i].font = openpyxl.styles.Font(italic=True, bold=True)
+
+
+## MANIPULATE COLUMNS
 
 cells = ws['A']
 color_counter = 0
+breaker = [first_data_row['cell']]
 
-
-
-
-breaker = [2]
-
-for i in range(2,rows):
-    cells[i].fill = openpyxl.styles.PatternFill(start_color=bg_color[color_counter], end_color=bg_color[color_counter], fill_type='solid')
+for i in range(first_data_row['index'],rows):
+    cells[i].fill = openpyxl.styles.PatternFill(start_color=bg_argb[color_counter], end_color=bg_argb[color_counter], fill_type='solid')
     try:
         if not checkValues(cells[i].value, cells[i+1].value):
-            color_counter = (color_counter+1) % 3
-            breaker.append()
+            color_counter = (color_counter+1) % bg_rgb_len
+            breaker.append(i+1+1) # add 1 for the next cell add 1 for index
     except:
         print('last row')
 
-    #print(cells[i].fill)
+breaker.append(i+1+1) # add the last row + 1
 
+
+for i in range(len(breaker)-1):
+    breaker_start = "A" + str(breaker[i])
+    breaker_end =   "A" + str(breaker[i+1]-1)
+    rng = breaker_start+':'+breaker_end
+    ws.merge_cells(rng)
+    ws.cell('breaker_start').alignment = openpyxl.styles.Alignment(horizontal='center')
 
 wb.save('output_test2.xlsx')
